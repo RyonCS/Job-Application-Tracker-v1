41376aff2c826cda28d36f194ebf2a986a5d5f5e
RyonCS <111393217+RyonCS@users.noreply.github.com>
2025-05-07 16:41:20 -0400

    Fixed to use process.env.PORT

--- Index.js
+++ Index.js
@@ -1,7 +1,7 @@
 import app from './app.js';
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Running our server.
 app.listen(port, () => {
-    console.log("Listening on Port 3000.")
+    console.log("Listening on Port ${port}.")
 })
